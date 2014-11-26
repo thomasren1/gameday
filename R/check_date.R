5980df4ac10ba6668ce2858f4325fbf2cd891563
thomasren1 <wyren1991@gmail.com>
2014-11-25 23:17:20 -0800

    vignettes update

--- R/check_date.R
+++ R/check_date.R
@@ -1,6 +1,5 @@
 ## unexported helper function
 # I wrote this function in a similar way that the internet_connection function is written.
-
 check_date <- function(date){
 	tryCatch({as.Date(date); TRUE},
 					 error = function(err) FALSE)
